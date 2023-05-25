cbc80cfdd432cd9c99268dc8b81b59faeb71614a
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-05-25 02:23:48 -0300

    initial repo

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from 'react'
+import './styles.scss'
+
+
+type HeaderProps = {
+
+}
+
+
+export default function Header( props: HeaderProps): ReactElement{
+
+
+    return(
+       <header>
+            <a href="#landing">
+                <span id="name">Lucas</span>
+                <span id="job-title">, developer</span>
+            </a>
+            <nav>        
+                <a href="#landing">Home</a>
+                <a href="#skills">Skills</a>
+                <a href="#portfolio">Portfolio</a>
+                <a href="#contact">Contact</a>
+            </nav>
+       </header>
+    )
+
+}
