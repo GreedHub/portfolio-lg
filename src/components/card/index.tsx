5cf79419d3d35afd6d0c8f668cabc715757ff7bd
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-05-25 02:58:43 -0300

    added card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react'
+import './styles.scss'
+
+export type CardProps = {
+}
+
+export default function Card(props:CardProps):ReactElement{
+    return(
+        <>
+        </>
+    )
+}
