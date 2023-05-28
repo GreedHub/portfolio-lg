442b25d4207275936fee70c5bf2dfd59b474ebdf
Lucas Julian Güerci <lucas.guerci@gmail.com>
2023-05-28 14:53:14 -0300

    added hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
-import Header from '../components/header'
+import Image from "next/image";
+import styles from "./page.module.scss";
+import Header from "../components/header";
+import Hero from "../components/hero";
 
 export default function Home() {
   return (
-   <>
-    <Header/>
-   </>
-  )
+    <>
+      <Header />
+      <div className="fake1"></div>
+      <div className="fake2"></div>
+      <main>
+        <Hero />
+      </main>
+    </>
+  );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,21 @@
-import { ReactElement } from 'react'
-import './styles.scss'
+import { ReactElement } from "react";
+import "./styles.scss";
 
+type HeaderProps = {};
 
-type HeaderProps = {
-
-}
-
-
-export default function Header( props: HeaderProps): ReactElement{
-
-
-    return(
-       <header>
-            <a href="#landing">
-                <span id="name">Lucas</span>
-                <span id="job-title">, developer</span>
-            </a>
-            <nav>        
-                <a href="#landing">Home</a>
-                <a href="#skills">Skills</a>
-                <a href="#portfolio">Portfolio</a>
-                <a href="#contact">Contact</a>
-            </nav>
-       </header>
-    )
-
+export default function Header(props: HeaderProps): ReactElement {
+  return (
+    <header>
+      <a href="#landing">
+        <span id="name">Lucas</span>
+        <span id="job-title">, developer</span>
+      </a>
+      <nav>
+        <a href="#landing">Home</a>
+        <a href="#skills">Skills</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
 }
